ebc23f37a228b7c84fcbe89f28c76d0b44de95f8
VonC <vonc@laposte.net>
2014-07-21 22:04:19 +0200

    Initial gitolite reader test.

---
+++ reader_test.go
@@ -0,0 +1,13 @@
+package gogitolite
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestRead(t *testing.T) {
+
+	Convey("An empty reader means no repos", t, func() {
+	})
+}
