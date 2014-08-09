7ccb2291e7d041ba33665f29ccfd42f103914a47
VonC <vonc@laposte.net>
2014-08-09 20:33:57 +0200

    Fix project example in project_test.go.

--- project_test.go
+++ project_test.go
@@ -17,18 +17,18 @@ var gitoliteconf = `
 	      -  VREF/NAME/                     = projectowner
 
 	    repo module1
-	      desc = module1 repo for repos group project1
 	      RW+ = projectowner @almadmins
-
-	    subconf "subs/*.conf"
-
-	    more subs/project.conf
-
-	    repo @project
-	      RW = projectowner @almadmins
-	      RW = otheruser1 otheruser2...
 `
 
+/*
+   subconf "subs/*.conf"
+
+   more subs/project.conf
+
+   repo @project
+     RW = projectowner @almadmins
+     RW = otheruser1 otheruser2...
+*/
 func TestProject(t *testing.T) {
 
 	Convey("An empty reader means no repos", t, func() {
@@ -36,6 +36,6 @@ func TestProject(t *testing.T) {
 		gtl, err := Read(r)
 		So(err, ShouldBeNil)
 		So(gtl.IsEmpty(), ShouldBeFalse)
-		So(gtl.NbRepos(), ShouldEqual, 1)
+		So(gtl.NbRepos(), ShouldEqual, 3)
 	})
 }
