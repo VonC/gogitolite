95f40153607c1626e4780d8efd57c4cadda81f62
VonC <VonC@laposte.net>
2014-09-08 09:25:54 +0200

    [gocyclo] contain cyclo complexity for goglt

--- goglt.go
+++ goglt.go
@@ -24,47 +24,61 @@ func main() {
 	var usersToRepos = make(map[string][]*gitolite.Repo)
 	for _, filename := range filenames {
 		fmt.Printf("Read file '%v'\n", filename)
-		f, err := os.Open(filename)
-		if err != nil {
-			fmt.Printf("ERR %v\n", err.Error())
-			os.Exit(1)
+		process(filename, usersToRepos)
+	}
+	print(usersToRepos)
+}
+
+func getGtl(filename string) (*os.File, *gitolite.Gitolite) {
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("ERR %v\n", err.Error())
+		os.Exit(1)
+	}
+	defer f.Close()
+	fr := bufio.NewReader(f)
+	gtl, err := reader.Read(fr)
+	if err != nil {
+		fmt.Printf("ERR %v\n", err.Error())
+		os.Exit(1)
+	}
+	return f, gtl
+}
+
+func updateUsersToRepos(user *gitolite.User, config *gitolite.Config, usersToRepos map[string][]*gitolite.Repo) {
+	var repos []*gitolite.Repo
+	var ok bool
+	if repos, ok = usersToRepos[user.GetName()]; !ok {
+		repos = []*gitolite.Repo{}
+	}
+	for _, cfgrepo := range config.GetRepos() {
+		seen := false
+		for _, repo := range repos {
+			if repo.GetName() == cfgrepo.GetName() {
+				seen = true
+				break
+			}
 		}
-		defer f.Close()
-		fr := bufio.NewReader(f)
-		gtl, err := reader.Read(fr)
-		if err != nil {
-			fmt.Printf("ERR %v\n", err.Error())
-			os.Exit(1)
+		if !seen {
+			repos = append(repos, cfgrepo)
 		}
-		// fmt.Println(gtl.String())
-		for _, config := range gtl.Configs() {
-			for _, rule := range config.Rules() {
-				if strings.Contains(rule.Access(), "R") {
-					for _, user := range rule.GetUsers() {
-						var repos []*gitolite.Repo
-						var ok bool
-						if repos, ok = usersToRepos[user.GetName()]; !ok {
-							repos = []*gitolite.Repo{}
-						}
-						for _, cfgrepo := range config.GetRepos() {
-							seen := false
-							for _, repo := range repos {
-								if repo.GetName() == cfgrepo.GetName() {
-									seen = true
-									break
-								}
-							}
-							if !seen {
-								repos = append(repos, cfgrepo)
-							}
-						}
-						usersToRepos[user.GetName()] = repos
-					}
+	}
+	usersToRepos[user.GetName()] = repos
+}
+
+func process(filename string, usersToRepos map[string][]*gitolite.Repo) *os.File {
+	f, gtl := getGtl(filename)
+	// fmt.Println(gtl.String())
+	for _, config := range gtl.Configs() {
+		for _, rule := range config.Rules() {
+			if strings.Contains(rule.Access(), "R") {
+				for _, user := range rule.GetUsers() {
+					updateUsersToRepos(user, config, usersToRepos)
 				}
 			}
 		}
 	}
-	print(usersToRepos)
+	return f
 }
 
 func print(usersToRepos map[string][]*gitolite.Repo) {
