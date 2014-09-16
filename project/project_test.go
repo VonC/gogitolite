919ecc322c62543fa969c18b7353ca3e7f44923d
VonC <VonC@laposte.net>
2014-09-16 10:42:44 +0200

    Test project without first RW rule (ignored)

--- project/project_test.go
+++ project/project_test.go
@@ -289,5 +289,30 @@ func TestProject(t *testing.T) {
 			So(pm.NbProjects(), ShouldEqual, 0)
 		})
 
+		Convey("No project if no RW rule first", func() {
+			var gitoliteconf = `
+		@project = module1 module2
+
+		repo gitolite-admin
+	      RW+     =   gitoliteadm @almadmins
+	      RW VREF/NAME/conf/subs/project    = projectowner
+	      -  VREF/NAME/                     = projectowner
+
+	    repo module1
+	      RW+ = projectowner @almadmins
+`
+			r := strings.NewReader(gitoliteconf)
+			gtl, _ := reader.Read(r)
+			subconfs := make(map[string]*gitolite.Gitolite)
+			subconfs["path/project.conf"] = gtl
+			pm := NewManager(gtl, subconfs)
+			//So(err, ShouldNotBeNil)
+			So(gtl, ShouldNotBeNil)
+			//So(strings.Contains(err.Error(), "group '@project' is a users group, not a repo one"), ShouldBeTrue)
+			So(gtl.IsEmpty(), ShouldBeFalse)
+			So(gtl.NbRepos(), ShouldEqual, 3)
+			So(pm.NbProjects(), ShouldEqual, 0)
+		})
+
 	})
 }
