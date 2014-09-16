c0b47f2668b3ce66f1d7709de71a8cbe7bde1c85
VonC <VonC@laposte.net>
2014-09-16 10:41:09 +0200

    Test project with no subconf (ignored)

--- project/project_test.go
+++ project/project_test.go
@@ -265,5 +265,29 @@ func TestProject(t *testing.T) {
 			So(pm.NbProjects(), ShouldEqual, 0)
 		})
 
+		Convey("No project if no subconfs", func() {
+			var gitoliteconf = `
+		@project = module1 module2
+
+		repo gitolite-admin
+	      RW+     =   gitoliteadm @almadmins
+	      RW                                = projectowner
+	      RW VREF/NAME/conf/subs/project    = projectowner
+	      -  VREF/NAME/                     = projectowner
+
+	    repo module1
+	      RW+ = projectowner @almadmins
+`
+			r := strings.NewReader(gitoliteconf)
+			gtl, _ := reader.Read(r)
+			pm := NewManager(gtl, nil)
+			//So(err, ShouldNotBeNil)
+			So(gtl, ShouldNotBeNil)
+			//So(strings.Contains(err.Error(), "group '@project' is a users group, not a repo one"), ShouldBeTrue)
+			So(gtl.IsEmpty(), ShouldBeFalse)
+			So(gtl.NbRepos(), ShouldEqual, 3)
+			So(pm.NbProjects(), ShouldEqual, 0)
+		})
+
 	})
 }
